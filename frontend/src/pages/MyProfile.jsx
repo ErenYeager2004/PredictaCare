c54bf2f3a1737ab2ec853b583381b44911babe46
Deyasin-Srijon <deyasin.srijon@gmail.com>
2025-03-11 15:14:58 +0530

    MyProfile UI updated

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -26,7 +26,7 @@ const MyProfile = () => {
       {
         isEdit
           ? <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
-          : <p>{userData.name}</p>
+          : <p className='font-medium text-3xl text-neutral-800 mt-4'>{userData.name}</p>
       }
       <hr className='bg-zinc-400 h-[1px] border-none'/>
       <div>
@@ -71,7 +71,7 @@ const MyProfile = () => {
           <p className='font-medium'>Birthday:</p>
           {
             isEdit ? <input className='max-w-28 bg-gray-100' type='date' onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
-              : <p className='text-gray-500'>{userData.dob}</p>
+              : <p className='text-gray-400'>{userData.dob}</p>
           }
         </div>
       </div>
