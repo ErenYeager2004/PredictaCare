b971bd84162e0ccd64ee37c814a1c00bda835d30
Deyasin-Srijon <deyasin.srijon@gmail.com>
2025-03-12 22:25:55 +0530

    DoctorList UI updated

--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -22,7 +22,7 @@ const DoctorList = () => {
                   <p className='text-neutral-800 text-lg font-medium'>{item.name}</p>
                   <p className='text-zinc-600 text-sm '>{item.speciality}</p>
                   <div className='mt-2 flex items-center gap-1 text-sm'>
-                    <input onChange={()=>changeAvailability(item._id)} type='checkbox' checked={item.available}/>
+                    <input onChange={()=>changeAvailability(item._id)} type='checkbox' checked={item.available} className="text-blue-600 accent-blue-600"/>
                     <p>Available</p>
                   </div>
                 </div>
