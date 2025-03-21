8ffe4e916f51ab4cedd8b7945fa3e706eae79eeb
Debjit Saha <debjitsaha606@gmail.com>
2025-03-21 21:08:57 +0530

    diagno ai ui improved

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
+import { toast } from 'react-toastify';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -13,6 +14,7 @@ const Navbar = () => {
         setToken(false)
         localStorage.removeItem('token')
         navigate('/')
+        toast("Logout Succesfully")
     }
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
