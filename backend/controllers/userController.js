30bc80bd568603a2a466a77c5ef545e15172e2df
Debjit Saha <debjitsaha606@gmail.com>
2025-06-08 19:21:04 +0530

    user model modiified

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -265,4 +265,5 @@ const verifyRazorpay = async (req, res) => {
   }
 }
 
+
 export { registerUser, loginUser, getProfile, updateProfile, bookAppointment,listAppointment,cancelAppointment,paymentRazorpay,verifyRazorpay}
