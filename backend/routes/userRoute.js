614a7eab44cbfb9559b7fc82290a9723778a4131
Debjit Saha <debjitsaha606@gmail.com>
2026-03-07 18:44:53 +0530

    fix(security): add strict rate limiting to all authentication routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,11 +3,12 @@ import express from 'express'
 import { registerUser,loginUser, getProfile,updateProfile,bookAppointment,listAppointment,cancelAppointment,paymentRazorpay,verifyRazorpay, refreshAccessToken} from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
+import authLimiter from '../middlewares/authLimiter.js'
 
 const userRouter = express.Router()
 
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
+userRouter.post('/register',authLimiter, registerUser)
+userRouter.post('/login',authLimiter, loginUser)
 userRouter.get('/get-profile',authUser,getProfile)
 userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment)
